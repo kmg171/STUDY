3194f7a853fda85187fa989ffdddaf7bd9047b4d
kmg171 <qweaiiok4781@gmail.com>
2025-09-04 13:21:12 +0900

    2일차 메모리 파일 추가

---
+++ 01.py
@@ -0,0 +1,31 @@
+#인터럽트 예제
+
+import time
+import signal
+
+def handler(signum, frame) :
+    print('키보드 인터럽트 감지')
+    print('신호 번호', signum)
+    print('신호 번호', frame)
+    exit()
+
+signal.signal(signal.SIGINT, handler)
+
+while True :
+    print('5초 간격으로 출력 중....')
+    time.sleep(5)#인터럽트 예제
+
+import time
+import signal
+
+def handler(signum, frame) :
+    print('키보드 인터럽트 감지')
+    print('신호 번호', signum)
+    print('신호 번호', frame)
+    exit()
+
+signal.signal(signal.SIGINT, handler)
+
+while True :
+    print('5초 간격으로 출력 중....')
+    time.sleep(5)
